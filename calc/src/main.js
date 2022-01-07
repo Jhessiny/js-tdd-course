c7e0b2a8436c007af92502a6e9cecbe255c934ae
jhessiny <jhessinymattos@gmail.com>
2022-01-07 00:00:01 -0300

    Add check-coverage to prepush hook

--- calc/src/main.js
+++ calc/src/main.js
@@ -1,14 +1,22 @@
-module.exports = {
-	sum(num1, num2) {
-		return num1 + num2;
-	},
-	sub(num1, num2) {
-		return num1 - num2;
-	},
-	div(num1, num2) {
-		return num2 === 0 ? "Not possible" : num1 / num2;
-	},
-	multi(num1, num2) {
-		return num1 * num2;
-	},
-};
+function sum(num1, num2) {
+	return num1 + num2;
+}
+function sub(num1, num2) {
+	return num1 - num2;
+}
+function div(num1, num2) {
+	return num2 === 0 ? "Not possible" : num1 / num2;
+}
+function multi(num1, num2) {
+	return num1 * num2;
+}
+
+function foo() {
+	return "oi";
+}
+
+function foo2() {
+	return "oi";
+}
+
+export { sum, sub, div, multi };
--- calc/tests/main.spec.js
+++ calc/tests/main.spec.js
@@ -1,57 +1,53 @@
 import { expect } from "chai";
-import calc from "../src/main";
+import { sub, sum, div, multi } from "../src/main";
 
 describe("Calc", function () {
 	describe("Smoke tests", function () {
-		it("should exist the calc lib", function () {
-			expect(calc).to.exist;
-		});
-
 		it("should exist the method sum", function () {
-			expect(calc.sum).to.exist;
-			expect(calc.sum).to.be.a("function");
+			expect(sum).to.exist;
+			expect(sum).to.be.a("function");
 		});
 
 		it("should exist the method sub", function () {
-			expect(calc.sub).to.exist;
-			expect(calc.sub).to.be.a("function");
+			expect(sub).to.exist;
+			expect(sub).to.be.a("function");
 		});
 
 		it("should exist the method div", function () {
-			expect(calc.div).to.exist;
-			expect(calc.div).to.be.a("function");
+			expect(div).to.exist;
+			expect(div).to.be.a("function");
 		});
 
 		it("should exist the method multi", function () {
-			expect(calc.multi).to.exist;
-			expect(calc.multi).to.be.a("function");
+			expect(multi).to.exist;
+			expect(multi).to.be.a("function");
 		});
 	});
 
 	describe("Sum", function () {
 		it("should return 4 when sum(2, 2)", () => {
-			expect(calc.sum(2, 2)).to.be.equal(4);
+			expect(sum(2, 2)).to.be.equal(4);
 		});
 	});
 
 	describe("Sub", function () {
 		it("should return 0 when sub(2, 2)", () => {
-			expect(calc.sub(2, 2)).to.be.equal(0);
+			expect(sub(2, 2)).to.be.equal(0);
 		});
 	});
 
 	describe("Div", function () {
 		it("should return 1 when div(2, 2)", () => {
-			expect(calc.div(2, 2)).to.be.equal(1);
+			expect(div(2, 2)).to.be.equal(1);
 		});
 		it('should return "not possible" when divided by 0', () => {
-			expect(calc.div(4, 0)).to.be.equal("Not possible");
+			expect(div(4, 0)).to.be.equal("Not possible");
 		});
 	});
 
 	describe("Multi", function () {
 		it("should return 4 when multi(2, 2)", () => {
-			expect(calc.multi(2, 2)).to.be.equal(4);
+			expect(multi(2, 2)).to.be.equal(4);
 		});
 	});
 });
